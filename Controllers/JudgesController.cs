d88f67d924fa9eb3b6d04b8a0476142175f1dddf
Javier Livio <JavierLivio@me.com>
2018-10-24 16:15:38 -0500

    judge controller - CRUD operations done.

--- Controllers/JudgesController.cs
+++ Controllers/JudgesController.cs
@@ -19,41 +19,30 @@ namespace aicupper.Controllers
             _context = context;
         }
 
-        // GET: Judges
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Judges.ToListAsync());
-        }
-
         [HttpGet("[action]")]
         public async  Task<IEnumerable<judge>> List()
         {
             return await _context.Judges.ToListAsync();
         }
 
-
+        [HttpGet("[action]/{id}")]
         // GET: Judges/Details/5
-        public async Task<IActionResult> Details(int id)
+        public async Task<judge> Details(int id)
         {
             if (id <= 0)
-            {
-                return NotFound();
-            }
+                throw new KeyNotFoundException($"Invalid id: {id}.");
 
-            var judge = await _context.Judges
-                .FirstOrDefaultAsync(m => m.JudgeID == id);
+            var judge = await _context.Judges.FirstOrDefaultAsync(m => m.JudgeID == id);
             if (judge == null)
-            {
-                return NotFound();
-            }
+                throw new KeyNotFoundException($"No Judge with id: {id} was found.");
 
-            return View(judge);
+            return judge;
         }
 
         // POST: Judges/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-         [HttpPost("[action]")]
+        [HttpPost("[action]")]
         public async Task<ActionResult> Create([FromBody] judge judgeObject)
         {
             if( judgeObject.EmailAddress.Length == 0 )
@@ -71,92 +60,52 @@ namespace aicupper.Controllers
                     return StatusCode(500);
                 }
             }
-            // return NotFound(new { Message = "Judge record cannot be created. Email is missing." });
         }
 
-        // GET: Judges/Edit/5
-        public async Task<IActionResult> Edit(int id)
-        {
-            if (id <= 0)
-            {
-                return NotFound();
-            }
-
-            var judge = await _context.Judges.FindAsync(id);
-            if (judge == null)
-            {
-                return NotFound();
-            }
-            return View(judge);
-        }
-
-        // POST: Judges/Edit/5
+        // PUT: Judges/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("JudgeID,AICupperAccountID,JudgeName,KnownAs,EmailAddress,PhoneNumber,CreatedOn,IsEnabled")] judge judge)
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] judge judge)
         {
-            if (id != judge.JudgeID)
-            {
-                return NotFound();
-            }
+            if (id <= 0)
+                throw new KeyNotFoundException($"Invalid id: {id}.");
 
-            if (ModelState.IsValid)
+            if (id != judge.JudgeID)
+                return NotFound(new { Message = $"Judge id: {id} does not match given data, id: {judge.JudgeID}." });
+
+            try 
             {
-                try
-                {
-                    _context.Update(judge);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!judgeExists(judge.JudgeID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                _context.Update(judge);
+                await _context.SaveChangesAsync();
+                return Accepted(new { Message = $"Judge record successfully updated id: {id}" });
             }
-            return View(judge);
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500);
+            }
         }
 
         // GET: Judges/Delete/5
+        [HttpDelete("[action]/{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             if (id <= 0)
-            {
-                return NotFound();
-            }
+                throw new KeyNotFoundException($"Invalid id: {id}.");
 
-            var judge = await _context.Judges
-                .FirstOrDefaultAsync(m => m.JudgeID == id);
+            var judge = await _context.Judges.FirstOrDefaultAsync(m => m.JudgeID == id);
             if (judge == null)
+               return NotFound(new { Message = $"Judge id: {id} not found." });
+            try
             {
-                return NotFound();
+                _context.Judges.Remove(judge);
+                await _context.SaveChangesAsync();            
+                return Accepted(new { Message = $"Judge record successfully deleted id: {id}" });
+            }   
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500);
             }
-
-            return View(judge);
-        }
-
-        // POST: Judges/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var judge = await _context.Judges.FindAsync(id);
-            _context.Judges.Remove(judge);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool judgeExists(int id)
-        {
-            return _context.Judges.Any(e => e.JudgeID == id);
         }
     }
 }
